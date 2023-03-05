18dae737616c45bfcb56390451938b4644282da6
Vitalij Bakalov <vbakalov@inno.co>
2023-03-05 08:54:34 +0700

    Added Layouts

---
+++ pages/cms-login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="login-page" />
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.login-page {
+  width: 100%;
+  height: 100%;
+  background: $purple-blue-gradient;
+}
+</style>
